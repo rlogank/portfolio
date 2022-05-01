b27ef785c71d681171bf6a75bca5a3f045b55c3d
Logan <98792682+LogansWebServices@users.noreply.github.com>
2022-05-01 19:09:02 -0400

    testing 2

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,7 +13,7 @@ const Nav = () => {
       className="absolute left-0 right-0 z-10 mx-auto flex w-full max-w-7xl items-center justify-between py-5 px-5 text-white"
     >
       <a href="/" className="text-3xl select-none font-extrabold tracking-tight">
-        {!open && "rlogankk"} 
+        {!open && "rlogank"} 
       </a>
 
       <div className="flex items-center justify-between text-lg font-medium">
